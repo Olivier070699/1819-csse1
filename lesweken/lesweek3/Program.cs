70926a3d4ec9d694c2419dfe579b346ea642cf88
Olivier Decock <olivdeco1@arteveldehs.be>
2018-10-10 11:58:55 +0200

    Lesweek 3

---
+++ lesweken/lesweek3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace lesweek3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // REKENMACHINE
+
+            int c, d;
+            int a = 5;
+            int b = 6;
+            int resultaat1 = optellen(int a, int b);
+            int resultaat2 = deling(int a, int b);
+            int resultaat3 = verhogen(int a);
+        }
+
+        static int optellen(int a, int b){
+            return a + b;
+        }
+
+        int verhogen(int a){
+            return a++;
+        }
+    }
+}
