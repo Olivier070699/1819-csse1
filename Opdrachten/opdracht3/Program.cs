af8500b786df9b4d9e703b9c1094b431483d9f89
Olivier Decock <olivdeco1@arteveldehs.be>
2018-11-07 13:29:11 +0100

    3

--- Opdrachten/opdracht3/Program.cs
+++ Opdrachten/opdracht3/Program.cs
@@ -5,8 +5,28 @@ namespace opdracht3
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {      
+        string[] lijst = new string[7];
+        lijst[0] = "Plat Water";
+        lijst[1] = "Cola 25 cl";
+        lijst[2] = "Cola 33 cl";
+        lijst[3] = "Witte Wijn";
+        lijst[4] = "Pils";
+        lijst[5] = "Toast";
+        lijst[6] = "Kaasplank";
+
+        string[] prijs = new string[7];
+        prijs[0] = "1";
+        prijs[1] = "1,50";
+        prijs[2] = "2";
+        prijs[3] = "3";
+        prijs[4] = "2";
+        prijs[5] = "5";
+        prijs[6] = "4";
+        
+        for(int i = 0; i <= 6; i++){
+            Console.WriteLine(lijst[i] + " kost " + prijs[i] + " euro.");
+        }
         }
     }
 }
