d62d9a41b39e705141fcd3f9d5dff67b9405b404
Olivier Decock <olivdeco1@arteveldehs.be>
2018-11-07 18:15:30 +0100

    3

--- Opdrachten/opdracht3/Program.cs
+++ Opdrachten/opdracht3/Program.cs
@@ -24,9 +24,27 @@ namespace opdracht3
         prijs[5] = "5";
         prijs[6] = "4";
         
+        Console.WriteLine("De kaart bevat volgende zaken:");
         for(int i = 0; i <= 6; i++){
             Console.WriteLine(lijst[i] + " kost " + prijs[i] + " euro.");
         }
+
+        Console.WriteLine("Wenst u te selecteren op naam, toets 1, of op volgnummer, toets 2?");
+        int antw = Int32.Parse(Console.ReadLine());
+            if(antw == 1){
+                Console.WriteLine("Geef de naam op van het gene dat u wenst.");
+                int name_antw = Int32.Parse(Console.ReadLine());
+                // AANVULLEN
+            }else if(antw == 2){
+                Console.WriteLine("Geef het volgnummer van het gene dat u wenst.");
+                int number_antw = Int32.Parse(Console.ReadLine());
+                int correct_number = number_antw - 1;
+                if(correct_number <= 6 && correct_number>=0){
+                    Console.WriteLine("U had graag een " + lijst[correct_number] + " gehad? Dat is dan " + prijs[correct_number] + " euro");
+                }else{
+                    Console.WriteLine("U heeft een onheldig nummer ingegeven.");
+                }
+            }
         }
     }
 }
