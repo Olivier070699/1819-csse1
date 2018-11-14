bef930ee9f9c827c3e86dbab1c089e03e58c8f28
Olivier Decock <olivdeco1@arteveldehs.be>
2018-11-14 19:25:37 +0100

    csse

---
+++ lesweken/lesweek5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lesweek5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Voertuig voertuig1 = new Voertuig)();
+            voertuig1.Laadvermogen = 12;
+            voertuig1.Kleur = "blauw";
+            Console.WriteLine(voertuig1)
+        }
+    }
+}
