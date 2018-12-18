7d9677a8cfc7016c9bab85acb034e7a698e92d37
Olivier Decock <olivdeco1@arteveldehs.be>
2018-12-18 14:04:19 +0100

    opdracht6

---
+++ Opdrachten/opdracht4/menu.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace opdracht4
+{
+    class menuItem{
+         public menuItem(string productNaam, string productType , string productIngrediënten){
+            
+        }
+    }    
+}
